e5b14655b0c6440b5ec82f17794653056b579fb4
STAGIAIRE <STAGIAIRE@2222-7.demostraining.com>
2017-10-13 16:40:48 +0200

    git fabrice vendredi

--- src/main/java/fr/demos/formation/septiemearche/metier/Commande.java
+++ src/main/java/fr/demos/formation/septiemearche/metier/Commande.java
@@ -3,17 +3,34 @@ package fr.demos.formation.septiemearche.metier;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 /**
  * @author Alexandre BIGOT
  *
  */
+@Entity
+@Table()
 public class Commande {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_article")
 
 	private Compte compte;
 	private String numCommande;
 	private String numFacture;
+	@Transient
 	private LocalDateTime dateCommande;
+	@OneToMany
 	private ArrayList<LigneCommande> lignesCommande = new ArrayList<LigneCommande>();
+	
+	public Commande() {}
 		
 	public Commande(Compte compte, String numCommande, String numFacture, LocalDateTime dateCommande,
 			ArrayList<LigneCommande> lignesCommande) {
