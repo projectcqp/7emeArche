7e32d9997e32996cb3549fa2b89cee548762c18f
STAGIAIRE <STAGIAIRE@2222-7.demostraining.com>
2017-11-21 16:37:55 +0100

    modif commentaire javadoc panier

--- src/main/java/fr/demos/formation/septiemearche/metier/Panier.java
+++ src/main/java/fr/demos/formation/septiemearche/metier/Panier.java
@@ -5,12 +5,19 @@ import java.util.Iterator;
 
 import fr.demos.formation.septiemearche.exceptions.ExceptionQuantiteDemandeeSuperieureAuStock;
 
+/**
+ * @author STAGIAIRE
+ * 
+ * Modèlise le panier du visiteur
+ * Un panier contient de 0 à plusieurs lignes d'articles
+ *
+ */
 public class Panier implements Iterable<LignePanier> {
-	
+
 	private Compte compte;
-//	relier un panier Ã  une session ou un compte
+	// relier un panier Ã  une session ou un compte
 	private ArrayList<LignePanier> lignesPanier = new ArrayList<>();
-	
+
 	
 	@Override
 	public String toString() {
@@ -19,146 +26,177 @@ public class Panier implements Iterable<LignePanier> {
 				+ getArticlesCumulesPanier() + "]";
 	}
 
-	public double getPrixTotal(){
-		double prixTotal=0;
-		double prixTotalArrondi=0.00; 
-		for (LignePanier ligneDuPanier : lignesPanier){
-			prixTotal = prixTotal + (ligneDuPanier.getQuantite() * ligneDuPanier.getArticle().getPrixHt());
-			prixTotalArrondi = Math.round(prixTotal*100.0)/100.0;
+	/**
+	 * Retourne le montant total du panier arrondis au centime
+	 * @return
+	 */
+	public double getPrixTotal() {
+		double prixTotal = 0;
+		double prixTotalArrondi = 0.00;
+		for (LignePanier ligneDuPanier : lignesPanier) {
+			prixTotal =+ (ligneDuPanier.getQuantite() * ligneDuPanier.getArticle().getPrixHt());
+			prixTotalArrondi = Math.round(prixTotal * 100.0) / 100.0;
 		}
 		return prixTotalArrondi;
 	}
-	
-	//sera appelÃ©e par ajouterUnArticle
-	private void ajouterLignePanier(Article a, int quantite) throws ExceptionQuantiteDemandeeSuperieureAuStock{
-		//si quantitÃ© demandÃ©e infÃ©rieure ou Ã©gale au stock
-		if (quantite <= a.getStock()){
+
+	/**
+	 * @param a
+	 * @param quantite
+	 * @throws ExceptionQuantiteDemandeeSuperieureAuStock
+	 * 
+	 * Ajoute l'article en argument pour la quantité demandée si le stock est disponible
+	 */
+	private void ajouterLignePanier(Article a, int quantite) throws ExceptionQuantiteDemandeeSuperieureAuStock {
+		// si quantitÃ© demandÃ©e infÃ©rieure ou Ã©gale au stock
+		if (quantite <= a.getStock()) {
 			lignesPanier.add(new LignePanier(a, quantite));
 		} else { // si quantite demandÃ©e supÃ©rieure au stock
-			if (a.getMaterialise() == null){ // si dÃ©materialise illimitÃ©, quantitÃ© toujours Ã  1
+			if (a.getMaterialise() == null) { // si dÃ©materialise illimitÃ©,
+												// quantitÃ© toujours Ã  1
 				lignesPanier.add(new LignePanier(a, quantite));
 			} else { // si matÃ©rialisÃ© stock insuffisant
-				throw new ExceptionQuantiteDemandeeSuperieureAuStock("Le stock n'est pas suffisant, nombre d'articles en stock : ", a.getStock());
-				}
+				throw new ExceptionQuantiteDemandeeSuperieureAuStock(
+						"Le stock n'est pas suffisant, nombre d'articles en stock : ", a.getStock());
+			}
 		}
 	}
-	
-	public void ajouterUnArticle(Article a, int quantiteAjoutee) throws ExceptionQuantiteDemandeeSuperieureAuStock{
+
+	public void ajouterUnArticle(Article a, int quantiteAjoutee) throws ExceptionQuantiteDemandeeSuperieureAuStock {
 		LignePanier lp1 = new LignePanier(a, quantiteAjoutee);
 		int indexDeMaLigne = lignesPanier.indexOf(lp1);
-		
-		// si la ligne a dÃ©jÃ  des articles il faut les comptabiliser pour comparer au stock
-		// test sur quantiteLignePlusQuantiteAjoutee, pas uniquement sur la quantitï¿½ ajoutï¿½e !!!
+
+		// si la ligne a dÃ©jÃ  des articles il faut les comptabiliser pour
+		// comparer au stock
+		// test sur quantiteLignePlusQuantiteAjoutee, pas uniquement sur la
+		// quantitï¿½ ajoutï¿½e !!!
 		int quantiteLignePlusQuantiteAjoutee = lp1.getQuantite() + quantiteAjoutee;
-		
+
 		// si la ligne article existe dÃ©jÃ 
-		if (indexDeMaLigne != -1){
+		if (indexDeMaLigne != -1) {
 			// si la quantitÃ© ajoutÃ©e <= stock de l'article
-			if (quantiteLignePlusQuantiteAjoutee <= a.getStock()){
-				lignesPanier.get(indexDeMaLigne).setQuantite(
-					lignesPanier.get(indexDeMaLigne).getQuantite() + quantiteAjoutee);
+			if (quantiteLignePlusQuantiteAjoutee <= a.getStock()) {
+				lignesPanier.get(indexDeMaLigne)
+						.setQuantite(lignesPanier.get(indexDeMaLigne).getQuantite() + quantiteAjoutee);
 			} else { // si quantite demandÃ©e supÃ©rieure au stock
-				 // si dÃ©materialise on s'en fiche c'est illimitÃ©
-				if (a.getMaterialise() == null){
-					lignesPanier.get(indexDeMaLigne).setQuantite(
-							lignesPanier.get(indexDeMaLigne).getQuantite() + quantiteAjoutee);
+				// si dÃ©materialise on s'en fiche c'est illimitÃ©
+				if (a.getMaterialise() == null) {
+					lignesPanier.get(indexDeMaLigne)
+							.setQuantite(lignesPanier.get(indexDeMaLigne).getQuantite() + quantiteAjoutee);
 				} else { // si matÃ©rialisÃ© et stock insuffisant
 					throw new ExceptionQuantiteDemandeeSuperieureAuStock(
 							"Le stock n'est pas suffisant, nombre d'articles en stock : ", a.getStock());
-					}
+				}
 			}
 		}
 		// si la ligne article n'existe pas
-		else { 
+		else {
 			this.ajouterLignePanier(a, quantiteAjoutee);
 		}
 	}
 
-	// supprimer ligne Ã  partir de la rÃ©fÃ©rence trouvÃ©e dans la ligne (c'est ma clÃ© primaire)
-	public void supprimerLignePanier(String refArticleLigne){
+	/**
+	 * @param refArticleLigne
+	 * 
+	 * Supprime la ligne du panier contenant la référence de l'article en argument 
+	 */
+	public void supprimerLignePanier(String refArticleLigne) {
 		Iterator<LignePanier> iter = lignesPanier.iterator();
-		while(iter.hasNext()){
-			LignePanier lp= iter.next();
+		while (iter.hasNext()) {
+			LignePanier lp = iter.next();
 			// si la rÃ©f est la mÃªme, c'est ma ligne panier
-			if(lp.getArticle().getReference().equals(refArticleLigne)){
+			if (lp.getArticle().getReference().equals(refArticleLigne)) {
 				iter.remove();
+				return;
 			}
 		}
 	}
-	
+
 	// TODO vÃ©rifier et utiliser
-	public boolean validerStockCommande(){
-		
+	public boolean validerStockCommande() {
+
 		boolean stockArticlesOk = true;
-		
+
 		// parcourir le panier et sortir la quantitÃ©
-		for(LignePanier ligne : lignesPanier){
-			
-			int nbreArticlesLigne = ligne.getQuantite();			
+		for (LignePanier ligne : lignesPanier) {
+
+			int nbreArticlesLigne = ligne.getQuantite();
 			// si au moins une lignePanier > stock article
-			if(nbreArticlesLigne <= ligne.getArticle().getStock()){
+			if (nbreArticlesLigne <= ligne.getArticle().getStock()) {
 				stockArticlesOk = false;
 			}
 		} // for each
 		return stockArticlesOk;
 	} // validerCommande
-	
-	
-	// nombre de lignes dans panier
-	public int getSizeContenuPanier(){
+
+	 /**
+	 * @return
+	 * 
+	 * retourne le nombre de ligne du panier
+	 */
+	public int getSizeContenuPanier() {
 		return lignesPanier.size();
 	}
-	
-	//nombre d'articles dans panier (plusieurs articles par ligne)
-	public int getArticlesCumulesPanier(){
+
+	/**
+	 * @return
+	 * 
+	 * Retourne le nombre d'articles cumulé du panier
+	 */
+	public int getArticlesCumulesPanier() {
 		int i = 0;
-		for(LignePanier lignePanier : lignesPanier){
-			i = i + lignePanier.getQuantite();		
+		for (LignePanier lignePanier : lignesPanier) {
+			i = i + lignePanier.getQuantite();
 		}
 		return i;
 	}
 
 	// methode pour modifier la quanttÃ© de la ligne panier
-	public void modifierQuantiteLignePanier (String refArticleLigne, int nouvelleQuantiteLigne) throws ExceptionQuantiteDemandeeSuperieureAuStock{
-		for(LignePanier lignePanier : lignesPanier){
+	public void modifierQuantiteLignePanier(String refArticleLigne, int nouvelleQuantiteLigne)
+			throws ExceptionQuantiteDemandeeSuperieureAuStock {
+		for (LignePanier lignePanier : lignesPanier) {
 			// si c'est bien mon article
-			if (lignePanier.getArticle().getReference().equals(refArticleLigne)){
-				// si article dÃ©matÃ©rialisÃ© (pas de stock Ã  gÃ©rer quantitÃ© illimitÃ©e)
-				if(lignePanier.getArticle().getMaterialise() == null){
+			if (lignePanier.getArticle().getReference().equals(refArticleLigne)) {
+				// si article dÃ©matÃ©rialisÃ© (pas de stock Ã  gÃ©rer quantitÃ©
+				// illimitÃ©e)
+				if (lignePanier.getArticle().getMaterialise() == null) {
 					lignePanier.setQuantite(nouvelleQuantiteLigne);
-				} else { // si article matÃ©riel 
+				} else { // si article matÃ©riel
 					// test quantitÃ© demandÃ©e et stock
-					if(nouvelleQuantiteLigne <= lignePanier.getArticle().getStock()) {
+					if (nouvelleQuantiteLigne <= lignePanier.getArticle().getStock()) {
 						lignePanier.setQuantite(nouvelleQuantiteLigne);
 					} else {
 						throw new ExceptionQuantiteDemandeeSuperieureAuStock(
-								"Le stock n'est pas suffisant, nombre d'articles en stock : ", lignePanier.getArticle().getStock());
+								"Le stock n'est pas suffisant, nombre d'articles en stock : ",
+								lignePanier.getArticle().getStock());
 					}
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * @param article
 	 * @param nouvelleQuantiteLigne
 	 * @throws ExceptionQuantiteDemandeeSuperieureAuStock
 	 */
-	public void modifierQuantiteLignePanier (Article article, int nouvelleQuantiteLigne) throws ExceptionQuantiteDemandeeSuperieureAuStock{
+	public void modifierQuantiteLignePanier(Article article, int nouvelleQuantiteLigne)
+			throws ExceptionQuantiteDemandeeSuperieureAuStock {
 		modifierQuantiteLignePanier(article.getReference(), nouvelleQuantiteLigne);
-}
-	
-	 /* 
+	}
+
+	/*
 	 * @param article
+	 * 
 	 * @return
 	 */
 	public boolean articleDejaDansLePanier(Article article) {
-for(LignePanier lignePanier : lignesPanier) {
-	if(lignePanier.getArticle() == article) {
-		return true;
-	}
-}
-return false;
+		for (LignePanier lignePanier : lignesPanier) {
+			if (lignePanier.getArticle() == article) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -173,54 +211,51 @@ return false;
 	public void setCompte(Compte compte) {
 		this.compte = compte;
 	}
-	
+
 	public double getTotalHt() {
 		double totalHt = 0;
-		
-		for(LignePanier lignePanier:lignesPanier) {
+
+		for (LignePanier lignePanier : lignesPanier) {
 			totalHt += lignePanier.getTotalHt();
 		}
-		
-		return totalHt; 
+
+		return totalHt;
 	}
-	
-		public double getTotalTc() {
-			double totalTc = 0;
-			
-			for(LignePanier lignePanier:lignesPanier) {
-				totalTc += lignePanier.getTotalTc();
-			}
-			
-			return totalTc;
-	}
-		
-		public double getMontantTva() {
-			double totalTva = 0;
-			
-			for(LignePanier lignePanier:lignesPanier) {
-				totalTva += lignePanier.getMontantTva();
-			}
-			
-			return totalTva;
+
+	public double getTotalTc() {
+		double totalTc = 0;
+
+		for (LignePanier lignePanier : lignesPanier) {
+			totalTc += lignePanier.getTotalTc();
 		}
-		
-		/**
-		 * Retourne la ligne du panier contenant l'article en paramètre
-		 * retourne null si elle n'existe pas.
-		 * 
-		 * @param article
-		 * @return
-		 */
-		public LignePanier getLignePanier(Article article) {
-			for(LignePanier lignePanier:lignesPanier) {
-				if(lignePanier.getArticle()== article) {
+
+		return totalTc;
+	}
+
+	public double getMontantTva() {
+		double totalTva = 0;
+
+		for (LignePanier lignePanier : lignesPanier) {
+			totalTva += lignePanier.getMontantTva();
+		}
+
+		return totalTva;
+	}
+
+	/**
+	 * Retourne la ligne du panier contenant l'article en paramètre retourne
+	 * null si elle n'existe pas.
+	 * 
+	 * @param article
+	 * @return
+	 */
+	public LignePanier getLignePanier(Article article) {
+		for (LignePanier lignePanier : lignesPanier) {
+			if (lignePanier.getArticle() == article) {
 				return lignePanier;
 			}
 		}
-			return null;
-		}
+		return null;
+	}
 
-		
-		
-		
 }
---
+++ src/test/java/fr/demos/formation/septiemearche/test/CommandeTest.java
@@ -0,0 +1,81 @@
+package fr.demos.formation.septiemearche.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.demos.formation.septiemearche.metier.Article;
+import fr.demos.formation.septiemearche.metier.Commande;
+import fr.demos.formation.septiemearche.metier.Etat;
+import fr.demos.formation.septiemearche.metier.Livre;
+import fr.demos.formation.septiemearche.metier.Panier;
+import fr.demos.formation.septiemearche.metier.Tva;
+
+
+/**
+ * @author STAGIAIRE
+ *
+ */
+public class CommandeTest {
+		private Panier panier;
+		private 	 Commande commande;
+	
+	@Before
+	public void initPanier() {
+		panier = new Panier();
+
+panier.ajouterUnArticle(new Livre("Reference", 10, "Nom", "UrlImage", new Tva(1, 10, "A"), 10, Etat.OCCASION_BON, "Auteur", "Isbn","Editeur", "Genre"), 1);
+///panier.ajouterUnArticle(new Livre("Reference", 13, "Nom", "UrlImage", new Tva(1, 10, "A"), "Format", "UrlDownload", "auteur", "Isbn", "Editeur", "enre";
+		///panier.ajouterUnArticle(a, quantiteAjoutee);new ArticleDivers("Reference", 10, "Nom", "UrlImage", new Tva(1, 10, "a"), 10, "nature");getClass()
+
+commande = new Commande(panier);
+	}
+
+	public CommandeTest(Article article) {}
+
+	/**
+	 * Test si le panier et la commande on le même nombre de lignes
+	 * 
+	 */
+	@Test
+	public void testNombreLignes() {
+		Assert.assertEquals(
+		panier.getSizeContenuPanier(),
+		commande.getSize()
+		);
+	}
+	
+	/**
+	 * Test si le panier et la commande on le même nombre d'article
+	 * 
+	 */
+	@Test
+	public void testNombreArticles() {
+		Assert.assertEquals(
+				panier.getArticlesCumulesPanier(),
+		commande.getNombreArticle());
+	}
+	
+	/**
+	 * Test si le montant total ht du panier  et de la commande sont identique
+	 * 
+	 */
+	@Test
+	public void testTotalHt() {
+		Assert.assertEquals(
+				panier.getTotalHt(),
+				commande.getTotalHt());
+	}
+	
+	/**
+	 * Test si le montant de la TVA du panier  et de la commande sont identiques
+	 * 
+	 */
+	@Test
+	public void testTotalTva() {
+		Assert.assertEquals(
+				panier.getMontantTva(),
+				commande.getMontantTva());
+	}
+	
+}
